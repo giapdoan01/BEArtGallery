2e13f6e6e135028b0900b5b4d6a77c3f8bf0ec87
giapdoan01 <126212968+giapdoan01@users.noreply.github.com>
2025-12-09 11:32:38 +0700

    Update ALLOWED_HOSTS in settings.py for deployment configuration

--- ArtGallery/ArtGallery/settings.py
+++ ArtGallery/ArtGallery/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-aq(h%b=uu(#0%ww8dd$k^*h@rqzn9r0atw^#nyrek7up%fib$5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
